2f1ce20874fa6bbd169626f9de8f8acae3b828bf
Jean-Louis Fuchs <ganwell@fangorn.ch>
2015-06-10 15:29:01 +0200

    Sources.list and authoriezed_keys

--- definitions/adsy-debian-7.7.0/adsy.sh
+++ definitions/adsy-debian-7.7.0/adsy.sh
@@ -1,2 +1,4 @@
+echo "deb http://mirror.switch.ch/ftp/mirror/debian/ wheezy-backports main" > \
+	/etc/apt/sources.list.d/backports.list
 aptitude -y install git rsync tmux python-pip python3-pip python-dev
 aptitude -y install python3-dev curl unzip vim
